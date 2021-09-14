3202485fe5acd9c29464175ba77f3e6cb55df2df
PradeepNarale <Pradeepnarale35@gmail.com>
2021-09-14 12:29:45 -0700

    initial commit

---
+++ mac/blog/views.py
@@ -0,0 +1,10 @@
+# Create your views here.
+from django.shortcuts import render
+from  django.http import HttpResponse
+def index(request):
+    return render(request,"blog/index.html")
+
+def blogpost(request):
+    return render(request,"blog/blogpost.html")
+
+
